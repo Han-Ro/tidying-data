d641c5277b79303add0f441a85803141f7323c01
Hannah Clark <hrc540@york.ac.uk>
2020-10-30 11:15:06 +0000

    summarising the data

--- task-1.R
+++ task-1.R
@@ -12,15 +12,34 @@ hdi <- read_csv(filehdi)
 str(hdi)
 #ignore columns 1 and 2 with 3:2
 hdi2 <- hdi %>% 
-  gather('year','hdi', 3:32)
+  gather('year','index', 3:32)
 str(hdi2)
 
 
+#filter out the country-year combos without results
+hdi_nona <- hdi2 %>% 
+  filter(!is.na(index))
+str(hdi_nona)
+
+
+#summarise the data
+hdi_summary <- hdi_nona %>% 
+  group_by(Country) %>% 
+  summarise(mean_index = mean(index),
+            n = length(index))
+hdi_summary
+#length is how many years there is data for
+
+#add a column for sd and se
+hdi_summary <- hdi_nona %>% 
+  group_by(Country) %>% 
+  summarise(mean_index = mean(index),
+            n = length(index),
+            sd_index = sd(index), 
+            se_index = (sd_index/(sqrt(n))))
 
 
 
 
 
 
-
-?gather
